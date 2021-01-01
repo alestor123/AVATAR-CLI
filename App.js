15dd8a70467ba0092cc3cc7834c139bc56ec4169
alestor123 <alestoraldous@gmail.com>
2021-01-01 12:11:00 +0530

    added main code

---
+++ App.js
@@ -0,0 +1,4 @@
+var got = require('got'),
+name = process.argv[2] || 'github',
+terminalImage = require('terminal-image');
+(async () => {console.log(await terminalImage.buffer(await got(`http://github.com/${name}.png`).buffer()));})();
